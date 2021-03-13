37973f187b21f5e803a7c33b773d4ade8df84c27
Ruxandra Matei <ruxandramatei0@gmail.com>
2021-03-13 16:32:15 +0200

    Created the project

---
+++ Smart-Lights-IoT-Project/cmake-build-debug/CMakeFiles/Smart_Lights_IoT_Project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Smart_Lights_IoT_Project.dir/main.cpp.o"
+  "Smart_Lights_IoT_Project"
+  "Smart_Lights_IoT_Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Smart_Lights_IoT_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
