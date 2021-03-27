620a99d4db389a6df67785c246a9d8beb94959b0
Anca-Maria <ancamariabrancoveanu@yahoo.com>
2021-03-27 15:51:15 +0200

    'bulb'

---
+++ Smart-Lights-IoT-Project/cmake-build-debug/CMakeFiles/Smart_Lights_IoT_Project.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/asus/Desktop/Anul 3/INGINERIA PROGRAMARII/Smart-Lights-IoT/Smart-Lights-IoT-Project/Buzzer.cpp" "/mnt/c/Users/asus/Desktop/Anul 3/INGINERIA PROGRAMARII/Smart-Lights-IoT/Smart-Lights-IoT-Project/cmake-build-debug/CMakeFiles/Smart_Lights_IoT_Project.dir/Buzzer.cpp.o"
+  "/mnt/c/Users/asus/Desktop/Anul 3/INGINERIA PROGRAMARII/Smart-Lights-IoT/Smart-Lights-IoT-Project/Lights.cpp" "/mnt/c/Users/asus/Desktop/Anul 3/INGINERIA PROGRAMARII/Smart-Lights-IoT/Smart-Lights-IoT-Project/cmake-build-debug/CMakeFiles/Smart_Lights_IoT_Project.dir/Lights.cpp.o"
+  "/mnt/c/Users/asus/Desktop/Anul 3/INGINERIA PROGRAMARII/Smart-Lights-IoT/Smart-Lights-IoT-Project/SmartLamp.cpp" "/mnt/c/Users/asus/Desktop/Anul 3/INGINERIA PROGRAMARII/Smart-Lights-IoT/Smart-Lights-IoT-Project/cmake-build-debug/CMakeFiles/Smart_Lights_IoT_Project.dir/SmartLamp.cpp.o"
+  "/mnt/c/Users/asus/Desktop/Anul 3/INGINERIA PROGRAMARII/Smart-Lights-IoT/Smart-Lights-IoT-Project/SmartLightController.cpp" "/mnt/c/Users/asus/Desktop/Anul 3/INGINERIA PROGRAMARII/Smart-Lights-IoT/Smart-Lights-IoT-Project/cmake-build-debug/CMakeFiles/Smart_Lights_IoT_Project.dir/SmartLightController.cpp.o"
+  "/mnt/c/Users/asus/Desktop/Anul 3/INGINERIA PROGRAMARII/Smart-Lights-IoT/Smart-Lights-IoT-Project/main.cpp" "/mnt/c/Users/asus/Desktop/Anul 3/INGINERIA PROGRAMARII/Smart-Lights-IoT/Smart-Lights-IoT-Project/cmake-build-debug/CMakeFiles/Smart_Lights_IoT_Project.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "PISTACHE_USE_SSL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "_deps/json-src/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
